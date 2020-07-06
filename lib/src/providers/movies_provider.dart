06480e52221207c2ac576e1de81a3a7b4f6dc689
Celia Navarro Pérez <celiinavarroperez@gmail.com>
2020-07-06 14:57:54 +0100

    Refactor : _processResponse()

--- lib/src/providers/movies_provider.dart
+++ lib/src/providers/movies_provider.dart
@@ -3,24 +3,30 @@ import 'dart:convert';
 import 'package:peliculas/src/models/movie_model.dart';
 
 class MoviesProvider {
-  String _apiKey   = '2c64d73c690b38dc1657735bc3ed8661';
-  String _url      = 'api.themoviedb.org';
+  String _apiKey = '2c64d73c690b38dc1657735bc3ed8661';
+  String _url = 'api.themoviedb.org';
   String _language = 'es-ES';
 
-
-  Future<List<Movie>> getNowInCinemas() async {
-    final url = Uri.https(_url, '3/movie/now_playing',{
-      'api_key'  : _apiKey,
-      'language' : _language
-    });
-
+  Future<List<Movie>> _processResponse(Uri url) async {
     final response = await http.get(url);
     final decodedData = json.decode(response.body);
 
     final movies = new Movies.fromJsonList(decodedData['results']);
 
     return movies.items;
-
   }
 
+  Future<List<Movie>> getNowInCinemas() async {
+    final url = Uri.https(_url, '3/movie/now_playing',
+        {'api_key': _apiKey, 'language': _language});
+
+    return await _processResponse(url);
+  }
+
+  Future<List<Movie>> getPopularMovies() async {
+    final url = Uri.https(
+        _url, '3/movie/popular', {'api_key': _apiKey, 'language': _language});
+
+    return await _processResponse(url);
+  }
 }
